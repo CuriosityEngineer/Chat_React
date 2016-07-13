7fa067544d0659ea582c2d3a5fe48125650ffd96
CuriosityEngineer <aeshta@gmail.com>
2016-07-13 22:10:05 +0200

    fixed welcome banner

--- src/containers/set-username.js
+++ src/containers/set-username.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import _ from 'lodash';
+import ColorSet from '../styles/ColorSet';
+import Fonts from '../styles/Fonts';
+import RaisedButton from 'material-ui/RaisedButton';
 
+const style = {
+  margin: 12,
+};
 
 class SetUsername extends React.Component{
+  constructor(props) {
+   super(props);
+
+   this.baseStyle = _.merge({
+     backgroundColor: ColorSet.blueGrey,
+     color: ColorSet.darkBlue,
+     fontFamily: Fonts.sansSerif,
+     position: "fixed",
+     top: 93,
+     left: 0,
+     right: 0,
+     margin: 0,
+     padding: "5px 10px",
+     boxShadow: "1px 1px 5px rgba(0,0,0,0.3)",
+   }, props.style || {});
+ }
   onSubmit(event){
     event.preventDefault();
 
@@ -13,10 +36,12 @@ class SetUsername extends React.Component{
 
   renderUserForm(){
     return(
+      <div style={ this.baseStyle}>
       <form onSubmit={this.onSubmit.bind(this)}>
         <input ref='newUsername' type='text' placeholder='What is your name?'/>
         <input type='submit' value='register'/>
       </form>
+      </div>
     )
   }
 
@@ -27,9 +52,13 @@ class SetUsername extends React.Component{
 
   renderGreeting() {
     return(
-      <div>
-        <p>Hi, {this.props.username}!</p>
-        (<a href="#" onClick={ this.resetUser.bind(this)}>Not you?</a>)
+      <div style={ this.baseStyle}>
+      <div style={{float: 'left', width: '200'}}>
+      <h3>Hi, {this.props.username}!</h3>
+      </div>
+      <div style={{float: 'right', paddingTop:15}}>
+      <RaisedButton label="Not You?"  onClick={ this.resetUser.bind(this)}/>
+      </div>
       </div>
     );
   }
